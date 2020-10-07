d81790e8262f53e7d7d37c5a8692e1e78bd18779
arhan kamra <arhankamra@yahoo.com>
2020-10-07 22:22:34 +0530

    add all files/folders

---
+++ root/README.md
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <meta charset='utf-8' />
+  </head>
+</html>
+
+Front-End-Internship-project
+============================
+About Project
+-------------
+
+This project was given to me during my first ever engineering internship interview
+What did they ask me?
+- [x] Css3
+- [ ] Html5
+>though they did ask on JavaScript
+>>so that was good
+
+| Css3 Performance     | JavaScript Performance     |
+| :-------------: | :-------------: |
+| `Excellent`       | ***Ok***       |
+
+### Address
+arhankamra@gmail.com
+[email me](mailto:arhankamra@gmail.com "clicking this will get you to your default email app")
+
+i am a :joy: in a camp
---
+++ root/main.js
@@ -0,0 +1,89 @@
+// 'use strict';
+//
+// let output = '';
+// function        getText() {
+//   let xhr = new XMLHttpRequest();
+//   xhr.onreadystatechange = function() {
+//     if( this.status === 200 && this.readyState == 4 ) {
+//       let response = this.responseText;
+//       let responseHeaders = this.getAllResponseHeaders();
+//       output += `<p>${response} ${responseHeaders}</p>`;
+//     }
+//     document.getElementById('get-text').innerHTML = output;
+//   }
+//
+//   xhr.open('GET', './simpleText.txt', true);
+//   xhr.send();
+// }
+//
+// function task(message) {
+//     // emulate time consuming task
+//     let n = 10000000000;
+//     while (n > 0){
+//         n--;
+//     }
+//     console.log(message);
+// }
+//
+// console.log('Start script...');
+// setTimeout(
+//   () =>
+//   task('Download a file.')
+//   ,0);
+// console.log('Done!');
+//
+// function power(num, pow) {
+//   if (pow == 1) {
+//   } else {
+//     return num;
+//     return num * power(num, pow-1);
+//   }
+// }
+//
+// console.log(power(5,3));
+//
+// const myObj = {};
+// let KEY1 = Symbol();
+// let KEY2 = Symbol('sym1');
+//
+//
+//
+//
+//
+// myObj.key3 = 'prop3';
+// myObj.key4 = 'prop4';
+// myObj[KEY1] = 'prop1';
+// myObj[KEY2] = 'prop2';
+//
+// console.log(myObj);
+//
+// for (let i in myObj) {
+//   console.log(`Only enumerable properties are: ${myObj[i]}`);
+//   console.log(JSON.stringify({ [Symbol()]: 'prop' }));
+//   console.log(JSON.stringify({ key2: 'prop' }));
+// }
+
+document.getElementById('button1').onclick = function() {
+  createButton();
+}
+
+// keep creating a button every 3 seconds and keep deleting it every 1.5 seconds
+const createButton = (function() {
+  let btn = document.createElement('BUTTON');
+  btn.innerHTML = 'button to click';
+  btn.setAttribute('class', 'button');
+  document.body.appendChild(btn);
+  let buttons = document.getElementsByClassName('button');
+  return function() {
+      for(let i = 0; i <= 10; i++) {
+          let btn = document.createElement('BUTTON');
+          btn.innerHTML = 'button to click' + ' ' + i;
+          btn.setAttribute('class', 'button');
+          document.body.appendChild(btn);
+          setInterval(() => buttons[i].remove(), 2000);
+    }
+  }})();
+// let button = document.getElementById('button');
+// button.onclick = function() {
+// document.getElementsByTagName('h1').innerText = 'simple heading'; }
+// let btnn = document.getElementById('button');
